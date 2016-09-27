e9ed8fbd904da55fe413288f6b8ed7fa3511299b
Coenraad Fick <coenraad@responsive.co.za>
2016-09-27 18:07:48 +0200

    Completed the Angular 2 Pro Pack up to excercise 11, Register Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
-import { ROUTES } from './app.routes';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { RacesComponent } from './races/races.component';
-import { RaceService } from './race.service';
-import { RaceComponent } from './race/race.component';
-import { PonyComponent } from './pony/pony.component';
-import { FromNowPipe } from './from-now.pipe';
-import { HomeComponent } from './home/home.component';
+import {ROUTES} from './app.routes';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {RacesComponent} from './races/races.component';
+import {RaceService} from './race.service';
+import {RaceComponent} from './race/race.component';
+import {PonyComponent} from './pony/pony.component';
+import {FromNowPipe} from './from-now.pipe';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MenuComponent,
-    RacesComponent,
-    RaceComponent,
-    PonyComponent,
-    FromNowPipe,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(ROUTES)
-  ],
-  providers: [
-    RaceService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MenuComponent,
+        RacesComponent,
+        RaceComponent,
+        PonyComponent,
+        FromNowPipe,
+        HomeComponent,
+        RegisterComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpModule,
+        RouterModule.forRoot(ROUTES)
+    ],
+    providers: [RaceService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
